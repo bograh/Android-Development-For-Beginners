b509eb8d28278cd488f60a2e075a0aba5aed340a
Bernard Ograh <baograh@st.knust.edu.gh>
2024-02-08 00:43:43 +0000

    Some Commit

--- ReactNative/MyFirstApp/App.js
+++ ReactNative/MyFirstApp/App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const Header = () => {
+    return (
+      <View style={styles.header}>
+        <Text>Header App</Text>
+      </View>
+    )
+  }
+
+  const Boxes = () => {
+    return 
+  }
+
+return (
+  <SafeAreaView style={styles.container}>
+    <Header />
+    <StatusBar style="auto" />
+  </SafeAreaView>
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  header: {
+    width: '100%',
+    height: '15%',
+    backgroundColor: '#8c8c8c',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
+
 });
